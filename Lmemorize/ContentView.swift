c6ac13c59459a9beaff09d8907f3f2704392a243
Leonardo Nogueira <nogueirahy@gmail.com>
2021-09-12 22:47:11 -0300

    Initial Commit

---
+++ Lmemorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lmemorize
+//
+//  Created by Leonardo Henrique Nogueira on 12/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lmemorize/LmemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LmemorizeApp.swift
+//  Lmemorize
+//
+//  Created by Leonardo Henrique Nogueira on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct LmemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
